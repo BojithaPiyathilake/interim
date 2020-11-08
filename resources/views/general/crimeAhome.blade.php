a452af416565225b791bb07949838c2a2f891438
Bojitha Piyathilake <bojithapiyathilake@gmail.com>
2020-11-08 16:27:37 +0530

    added sharukas stuff not working yet

---
+++ resources/views/general/crimeAhome.blade.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Dark Dashboard</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  <style>
+    .inline{
+        display: inline;
+      }
+  </style>
+</head>
+
+<body class="bg-secondary">
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <!-- Brand/logo -->
+        <a class="navbar-brand mr-auto" href="#">
+        <img src="Logo.jpeg" alt="logo" style="width:70px;">
+        </a>
+
+        <a href="#" class="text-secondary mr-1">Help          |</a>
+
+        <a href="#" class="text-secondary">
+            <i class="fas fa-bell mr-1"> <span class="badge badge-light">4</span>        |</i>
+        </a>
+
+        <a href="#" class="text-secondary">
+            <i class="fas fa-cog mr-1">          |</i>
+        </a>
+
+
+        <ul class="navbar-nav">
+        <!-- Authentication Links -->
+        @guest
+        <li class="nav-item">
+            <a class="nav-link text-light" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+        @if (Route::has('register'))
+            <li class="nav-item">
+                <a class="nav-link text-light" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+        @endif
+        @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        @endguest
+        </ul>
+    </nav>
+
+
+
+<br>
+
+<div class="container-fluid">
+    <div class="row">
+    <div class="col-lg-2">      <!--if screen size goes below large then start stacking-->
+        <div class="p-2 border bg-dark border-dark rounded-lg flex-shrink-3">
+            <h4 class="p-2 border border-light rounded-lg text-white">Navigation Bar</h5>
+            <!-- A vertical navbar -->
+            <nav class="navbar bg-dark">
+            <!-- Links -->
+            <ul class="navbar-nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link text-light font-italic p-2" href="/general">General Module</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light font-italic p-2" href="/dashboard/admin">User Management</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light font-italic p-2" href="#">Environment Module</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light font-italic p-2" href="#">Admin Module</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light font-italic p-2" href="#">Security Module</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light font-italic p-2" href="#">Update Map</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light font-italic p-2" href="#">Requests</a>
+            </li>
+            </ul>
+            </nav>
+        </div>
+    </div>
+
+
+    <div style="background-color:#ECF0F1" class="col-md p-2 border border-secondary rounded-lg ml-2 mr-3">
+    <table class="table table-dark table-striped border-secondary rounded-lg mr-4">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>crime type</th>
+                <th>description</th>
+                <th>Location</th>
+                <th>Date complained logged</th>
+                <th>Action taken</th>
+                <th>Status</th>
+                <th>Assign</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($Crimes as $row) <tr>
+                <td>{{$row['id']}}</td>
+                <td>{{$row['crime_type']}}</td>
+                <td>{{$row['description']}}</td>
+                <td>{{$row['polygon']}}</td>
+                <td>{{$row['created_at']}}</td>
+                <td>{{$row['action_taken']}}</td>
+                <td>{{$row['status']}}</td>
+                <td><a href='/assign/{{ $row['id'] }}' class="btn btn-outline-warning" role="button" >Assign</a></td>
+                <!-- <td><a href="#" class="btn btn-outline-danger" role="button">Disable</a></td> -->
+                </tr>
+                @endforeach
+        </tbody>
+    </table>
+    </div>
+</div>
+</div>
+<br>
+<div class="d-flex  bg-light justify-content-end">
+  <a href="#" class="text-secondary mr-2">
+    <i class="fab fa-facebook-square"></i> Facebook |
+  </a>
+  <a href="#" class="text-secondary mr-2">
+    <i class="fab fa-twitter-square"></i> Twitter |
+  </a>
+  <a href="#" class="text-secondary mr-2">
+    <i class="fab fa-instagram"></i>  Instagram
+  </a>
+</div>
+<div class="d-flex bg-light justify-content-center">
+  <h5 class="text-secondary"><i class="far fa-copyright"></i> 2020 by Reforest Sri Lanka</h5><br>
+</div>
+<div class="d-flex bg-light justify-content-center">
+  <h6>All rights reserved</h6>
+</div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\Crime_reportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -101,3 +102,25 @@ Route::get('/manager/create', function () {
      });
 
 Route::post('/manager/store', 'managerController@store');
+
+
+
+
+
+
+
+//////////////////GENERAL MoDULE
+
+// Route::get('/index', [Crime_reportController::class, 'index']);
+Route::post('/crimecreate', [Crime_reportController::class, 'create']);
+Route::get('/crimehome', fn() => view('general.crimeChome'));
+Route::get('/newcrime', fn() => view('general.logComplaint'));
+Route::get('/general', fn() => view('general.general'));
+Route::get('/trackcrime', [Crime_reportController::class, 'track']);
+Route::get('/crimeadmin', [Crime_reportController::class, 'admin']);
+//Route::get('/crimehome', [Crime_reportController::class, 'crimehome']);
+//Route::get('{id}', 'crime_reportController@edit')->name('edit');
+Route::get('/assign/{id}',[Crime_reportController::class, 'show']);
+Route::get('crimeassign',fn() => view('general.crimeAssign'));
+Route::get('/searchauth', [Crime_reportController::class, 'searchauth']);
+Route::post('/assignauth', [Crime_reportController::class, 'assignauth']);
